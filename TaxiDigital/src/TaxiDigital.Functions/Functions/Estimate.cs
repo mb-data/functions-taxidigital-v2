77a352c2dfbb4982127b997c2c6c70b017023bc7
Luís Chalupa <oeternojunior@gmail.com>
2025-01-10 16:52:26 +0000

    #PW-2322
    Refactor Estimate class and update Run method
    
    Updated using directives to include necessary namespaces and removed unused ones. Modified the Estimate constructor to include ISender sender and added a private readonly field _sender. Changed the return type of the Run method to Task<List<EstimativeQueueRequest>> and implemented sending an EstimateCommand using _sender.Send(command). The result of the command is now returned in the Run method.

--- TaxiDigital/src/TaxiDigital.Functions/Functions/Estimate.cs
+++ TaxiDigital/src/TaxiDigital.Functions/Functions/Estimate.cs
@@ -1,20 +1,22 @@
-using System;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
+using MediatR;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using TaxiDigital.Application.UseCases.Estimate;
+using TaxiDigital.Domain.Estimate.Requests;
 using TaxiDigital.Functions.Messages;
 
 namespace TaxiDigital.Functions.Functions;
 
-public class Estimate(ILogger<Estimate> logger)
+public class Estimate(ILogger<Estimate> logger, , ISender sender)
 {
     private readonly ILogger<Estimate> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    private readonly ISender _sender = sender ?? throw new ArgumentNullException(nameof(sender));
 
     [Function(nameof(Estimate))]
     [ServiceBusOutput("estimate-taxidigital-queue", Connection = "ServiceBusConnection")]
-    public async Task Run(
+    public async Task<List<EstimativeQueueRequest>> Run(
         [ServiceBusTrigger("estimate-taxidigital", Connection = "ServiceBusConnection")]
         ServiceBusReceivedMessage message,
         ServiceBusMessageActions messageActions)
@@ -24,9 +26,11 @@ public class Estimate(ILogger<Estimate> logger)
         var messageBody = message.Body.ToString();
         var estimateMessage = JsonSerializer.Deserialize<EstimateMessage>(messageBody);
 
-
+        var command = new EstimateCommand(estimateMessage.RideId);
+        var result = await _sender.Send(command);
 
         // Complete the message
         await messageActions.CompleteMessageAsync(message);
+        return result.Value;
     }
 }
