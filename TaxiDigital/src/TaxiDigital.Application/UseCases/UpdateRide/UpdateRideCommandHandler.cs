bdbe24e6ecf43eb9b7d7c066207d43cdf68ac9df
Luís Chalupa <oeternojunior@gmail.com>
2025-01-27 22:02:48 +0000

    PW-2324
    Add UpdateRide command, handler, and Azure Function
    
    Introduce UpdateRideCommand and handler in UseCases namespace.
    Create UpdateRide Azure Function to process Service Bus messages.
    Add UpdateRideMessage record type for deserialization.

---
+++ TaxiDigital/src/TaxiDigital.Application/UseCases/UpdateRide/UpdateRideCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiDigital.Application.Common.Messaging;
+using TaxiDigital.SharedKernel;
+
+namespace TaxiDigital.Application.UseCases.UpdateRide;
+
+internal sealed class UpdateRideCommandHandler : ICommandHandler<UpdateRideCommand>
+{
+    public Task<Result> Handle(UpdateRideCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
